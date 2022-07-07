71a7e3e0f776426548953840c017c09304ca8ed9
miasif <minhazul.islam.asif@gmail.com>
2022-07-07 14:35:46 +0600

    header complete

--- src/pages/HotelList/HotelList.jsx
+++ src/pages/HotelList/HotelList.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
+import Navbar from '../../components/Navbar/Navbar';
 
 export default function HotelList() {
-  return <div>HotelList</div>;
+  return (
+    <div>
+      <Navbar />
+      <Header type="list" />
+    </div>
+  );
 }
