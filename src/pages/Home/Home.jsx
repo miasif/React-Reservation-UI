279c6ebd7e07cf92af6de66f1b44475b74089314
miasif <minhazul.islam.asif@gmail.com>
2022-12-22 01:53:38 +0600

    propertylist done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Featured from '../../components/Featured/Featured';
 import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar';
+import PropertyList from '../../components/propertyList/PropertyList';
 import './Home.css';
 
 function Home() {
@@ -12,6 +13,7 @@ function Home() {
       <div className="homeContainer">
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
       </div>
     </>
   );
