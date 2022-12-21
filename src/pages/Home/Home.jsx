f23fcd820a79e5bebfdc17cbb2c624dd0f802c07
miasif <minhazul.islam.asif@gmail.com>
2022-12-22 02:27:11 +0600

    mailList done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Featured from '../../components/Featured/Featured';
 import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties';
 import Header from '../../components/Header/Header';
+import MailList from '../../components/mailList/MailList';
 import Navbar from '../../components/Navbar/Navbar';
 import PropertyList from '../../components/propertyList/PropertyList';
 import './Home.css';
@@ -18,6 +19,7 @@ function Home() {
         <h1 className="homeTitle">Homes guests love</h1>
 
         <FeaturedProperties />
+        <MailList />
       </div>
     </>
   );
