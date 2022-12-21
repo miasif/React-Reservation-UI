5d017858cd9d377195347e43d3e6b578c3415ec5
miasif <minhazul.islam.asif@gmail.com>
2022-12-22 02:15:06 +0600

    featureProperties done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Featured from '../../components/Featured/Featured';
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties';
 import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar';
 import PropertyList from '../../components/propertyList/PropertyList';
@@ -14,6 +15,9 @@ function Home() {
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+
+        <FeaturedProperties />
       </div>
     </>
   );
