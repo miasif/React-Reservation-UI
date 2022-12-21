94e07024487e043f98873d5982a697e545198673
Asif <minhazul.islam.asif@gmail.com>
2022-12-22 03:34:48 +0600

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Featured from '../../components/Featured/Featured';
-import FeaturedProperties from '../../components/FeaturedProperties/FeaturedProperties';
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import MailList from '../../components/MailList/MailList';
